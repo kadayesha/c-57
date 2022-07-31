2dbcdc52b8ebbeb580f98c6687d759e13e6d46fd
kadayesha <93032523+kadayesha@users.noreply.github.com>
2022-07-31 19:52:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("my first console project");
+var firstname="kadayesha ";
+var lastname="tyagi";
+var fullname=firstname.concat(lastname);
+console.log(fullname);
+var a=2;
+var b=7;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
